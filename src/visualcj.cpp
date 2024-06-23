3755c2b6f168f1340b0d6ac29b697cc10a0c57b3
Martin Dunn <martin.dun176@hotmail.com>
2024-06-23 21:24:03 +0100

    update hb prevent

--- src/visualcj.cpp
+++ src/visualcj.cpp
@@ -271,6 +271,11 @@ void VCJ_onClientMoveCommand(client_t *client, usercmd_t *ucmd)
         if(fabs(VCJ_xvel[clientNum]) < fabs(client->gentity->client->ps.velocity[0]) && fabs(client->gentity->client->ps.velocity[0]) > 300 && ucmd->forwardmove == 0 && (ucmd->rightmove == -127 || ucmd->rightmove == 127))
         {
             //posssible hb in x dir
+            if(!VCJ_playerHalfbeatPermissions[clientNum])
+            {
+                client->gentity->client->ps.velocity[0] = VCJ_xvel[clientNum];
+            }
+
             int callback = VCJ_callbacks[VCJ_CB_PLAYER_HB];
             if(callback)
             {
@@ -283,6 +288,11 @@ void VCJ_onClientMoveCommand(client_t *client, usercmd_t *ucmd)
         if(fabs(VCJ_yvel[clientNum]) < fabs(client->gentity->client->ps.velocity[1]) && fabs(client->gentity->client->ps.velocity[1]) > 300 && ucmd->forwardmove == 0 && (ucmd->rightmove == -127 || ucmd->rightmove == 127))
         {
             //posssible hb in x dir
+            if(!VCJ_playerHalfbeatPermissions[clientNum])
+            {
+                client->gentity->client->ps.velocity[1] = VCJ_yvel[clientNum];
+            }
+
             int callback = VCJ_callbacks[VCJ_CB_PLAYER_HB];
             if(callback)
             {
